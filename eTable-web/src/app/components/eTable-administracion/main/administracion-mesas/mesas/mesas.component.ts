5f97d13940bdd822220e39c6fdb81d2f3bddde09
NatalyGVS <nataly.vasquez9718@gmail.com>
2019-11-17 17:58:56 -0500

    ...

--- eTable-web/src/app/components/eTable-administracion/main/administracion-mesas/mesas/mesas.component.ts
+++ eTable-web/src/app/components/eTable-administracion/main/administracion-mesas/mesas/mesas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-mesas',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mesas.component.css']
 })
 export class MesasComponent implements OnInit {
+  estado: boolean;
 
-  constructor() { }
+   constructor(private router: Router) {
+     this.estado = false;
+    }
 
   ngOnInit() {
+  }
+
+  
+  cambiarEstadoMesa(id: number, estado: boolean) {
+    this.estado = !this.estado;
+  }
+
+  nuevaMesa() {
+    this.router.navigate(['mesas/crear']);
   }
 
 }
