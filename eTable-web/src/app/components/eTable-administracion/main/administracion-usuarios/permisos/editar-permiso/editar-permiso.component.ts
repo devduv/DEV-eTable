ecce21a2761f9e72940fbe1de2b32f8bb88cd36e
iSeth1998 <duvanbradbrid@gmail.com>
2019-11-24 00:38:33 -0500

    Ahora se puede visualizar los permisos de usuario

--- eTable-web/src/app/components/eTable-administracion/main/administracion-usuarios/permisos/editar-permiso/editar-permiso.component.ts
+++ eTable-web/src/app/components/eTable-administracion/main/administracion-usuarios/permisos/editar-permiso/editar-permiso.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { PermisosService } from 'src/app/services/administracion/administracion-usuarios/permisos.service';
 import { Permiso } from 'src/app/domain/Permiso';
+import { Path } from 'src/app/infrastructure/constans/Path';
 
 @Component({
   selector: 'app-editar',
@@ -10,12 +11,14 @@ import { Permiso } from 'src/app/domain/Permiso';
 })
 export class EditarPermisoComponent implements OnInit {
 
-  public permiso: Permiso;
-
-  duvan: boolean;
+  permiso: Permiso;
+  load: boolean;
+  loading: string;
 
   constructor(private router: Router, private service: PermisosService) {
     this.permiso = new Permiso();
+    this.load = true;
+    this.loading = Path.loading;
    }
 
   ngOnInit() {
@@ -29,7 +32,10 @@ export class EditarPermisoComponent implements OnInit {
 
   getPermisoById(id: number) {
     this.service.getPermisoById(id).subscribe(data => {
-      this.permiso = data;
+      if (data !== null) {
+        this.load = false;
+        this.permiso = data;
+      }
     });
   }
 
