d897c612800bf6231c5dd5c22e11451767d9a730
NatalyGVS <nataly.vasquez9718@gmail.com>
2019-12-11 23:36:05 -0500

    avance perfil mesa

--- eTable-web/src/app/services/administracion/administracion-mesas/mesas.service.ts
+++ eTable-web/src/app/services/administracion/administracion-mesas/mesas.service.ts
@@ -5,18 +5,14 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
-export class PerfilMesasService {
+export class MesasService {
 
   private url: string;
   constructor(private http: HttpClient) {
     this.url = 'etable/api/perfilMesa';
   }
 
-  getPerfilesMesa() {
-    return this.http.get<PerfilMesa[]>(this.url + '/' + 'listPerfilMesas');
-  }
-
-  public crearPerfilMesa (perfilMesa: PerfilMesa) {
-    return this.http.post<PerfilMesa>(this.url + '/' + 'perfilMesa', perfilMesa);
-  }
+  // getPerfilesMesa() {
+  //   return this.http.get<PerfilMesa[]>(this.url + '/' + 'perfiles');
+  // }
 }
